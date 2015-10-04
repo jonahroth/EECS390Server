1458adc0723d76f39de11eb30c80be665c1df69c
Jonah Raider Roth <jer135@case.edu>
2015-10-04 13:19:11 -0400

    Heroku compatibility

--- config/environments.rb
+++ config/environments.rb
@@ -6,8 +6,8 @@ configure :production, :development do
 	ActiveRecord::Base.establish_connection(
 			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
 			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
+			:username => (ENV['PG_USER'] || db.user),
+			:password => (ENV['PG_PASS'] || db.password),
 			:database => db.path[1..-1],
 			:encoding => 'utf8'
 	)
