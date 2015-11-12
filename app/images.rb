dd2921d537450119c50759afaf341141f6eb9434
Jonah Raider Roth <jer135@case.edu>
2015-11-12 09:09:47 -0500

    Added images for powerups

--- app/images.rb
+++ app/images.rb
@@ -11,3 +11,13 @@ get '/img/w/:identifier' do
     nil.to_json
   end
 end
+
+get '/img/p/:identifier' do
+  p = Powerup.find_by(:identifier => params[:identifier])
+  if p
+    send_file "./assets/img/powerups/#{p.path}"
+  else
+    content_type :json
+    nil.to_json
+  end
+end
